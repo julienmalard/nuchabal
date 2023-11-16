676cb637e04c5278e4e348cb861be5aae7b0bd25
julienmalard <julien.malard@mail.mcgill.ca>
2023-11-16 16:55:55 +0100

    version

--- js/ruxeel/jantape.ts
+++ js/ruxeel/jantape.ts
@@ -2,7 +2,8 @@ export const jaqbälKikajtzïkChabäl = "chab'äl";
 export const jaqbälKikajtzïkTzibanem = "tzib'anem";
 
 // K'o chi tiqajal konojel rochochib'äl richin orbit-db
-export const runukMolajKel = "orbitdb/zdpuAqGK5ndjkaYFM5wkyEk4S6FpDH5hvpEWLq79edP9MmMpY";
+export const runukMolajKel = 
+  "/orbitdb/zdpuB2ZNKMRPnn8kwwL9UDY6VNdXxqxTUiEanjJ4Z6HuxEgWU";
 
 export const retalTzibNuchabäl =
   "/orbitdb/zdpuAqGK5ndjkaYFM5wkyEk4S6FpDH5hvpEWLq79edP9MmMpY";
@@ -18,21 +19,21 @@ export const runukRucheelRunukI2Chabäl = "i2";
 export const runukRucheelRunukI3Chabäl = "i3";
 
 export const runukRujalojRunukChabäl =
-  "/orbitdb/zdpuAqAZSmXwDoEPn4r94LP7yYvaY6kvxofyU2gchbAzkTEjN/7affb315-cbda-471b-ab38-4b510be31f80";
+  "/orbitdb/zdpuAqAZSmXwDoEPn4r94LP7yYvaY6kvxofyU2gchbAzkTEjN";
 export const runukRujalojRajilanïkChabäl =
-  "/orbitdb/zdpuAx2asNr8YFLvEy8nP5u16SshcbMAhyHkZQm3hqEX2CEP6/c6a6924c-4b92-4a7f-b259-eb725e97706e";
+  "/orbitdb/zdpuAx2asNr8YFLvEy8nP5u16SshcbMAhyHkZQm3hqEX2CEP6";
 export const runukRujalojRubiChabäl =
-  "/orbitdb/zdpuAxRf1zitq3gpB6BnMrSuwcMLeFEUuUUGZZyhCwhjNqUjM/23617e18-1cd6-4e98-9eb8-f779c6ac54db";
+  "/orbitdb/zdpuAxRf1zitq3gpB6BnMrSuwcMLeFEUuUUGZZyhCwhjNqUjM";
 export const runukRujalojRutzibanemChabäl =
-  "/orbitdb/zdpuAp5wKQEHAh8L5VGJr4Jdzn5363rX6zZmeFawBS4vRBihV/2c33ece9-a19c-4713-aac0-104478e142ae";
+  "/orbitdb/zdpuAp5wKQEHAh8L5VGJr4Jdzn5363rX6zZmeFawBS4vRBihV";
 export const runukRujalojRunukGlChabäl =
-  "/orbitdb/zdpuApxEECg9CrBGKtGqkYMU4zU51q6M1UuRvzz1PhQ5XTQWt/ed95ff2b-0f30-41a4-8efd-92196b65dab7";
+  "/orbitdb/zdpuApxEECg9CrBGKtGqkYMU4zU51q6M1UuRvzz1PhQ5XTQWt";
 export const runukRujalojRunukI1Chabäl =
-  "/orbitdb/zdpuApSr92X1P9KQS5EzJVrWTqA8RZ4rjZnPJwH5DTNmw35CY/62569dba-f8d3-4c7a-ba3d-d07fac9d55e0";
+  "/orbitdb/zdpuApSr92X1P9KQS5EzJVrWTqA8RZ4rjZnPJwH5DTNmw35CY";
 export const runukRujalojRunukI2Chabäl =
-  "/orbitdb/zdpuAv9jLJJzgo5FafC3ZeG2nGggEWttcb8hMbfwwbRQKW1DS/43cd5ea5-8080-4d55-b5a1-8b782509423f";
+  "/orbitdb/zdpuAv9jLJJzgo5FafC3ZeG2nGggEWttcb8hMbfwwbRQKW1DS";
 export const runukRujalojRunukI3Chabäl =
-  "/orbitdb/zdpuApqihbDFFCdHudZaM64ahUMDwkpb38fpjw5gqYkP4xGJC/430bb19c-b541-4b88-85a0-e4ef9b8b9b29";
+  "/orbitdb/zdpuApqihbDFFCdHudZaM64ahUMDwkpb38fpjw5gqYkP4xGJC";
 
 // Tzib'anem
 export const runukRucheelRunukTzib = "runukTzib";
@@ -42,12 +43,12 @@ export const runukRucheelRucholanemTzib = "rucholanemTzib";
 export const runukRucheelRutzibTzib = "rutzibTzib";
 
 export const runukRujalojRunukTzib =
-  "/orbitdb/zdpuB1o5fEEZFicWiQTL7RE8xv7v7X2SS5A7tNUAXaHQ9c58P/5e71fe3b-3eb9-4574-b2b5-d8d9f3f82a63";
+  "/orbitdb/zdpuB1o5fEEZFicWiQTL7RE8xv7v7X2SS5A7tNUAXaHQ9c58P";
 export const runukRujalojRajilanïkTzib =
-  "/orbitdb/zdpuAt4noCPbhfzRAZncCMndUPSCiYS5KnHDCPg17RFqV8qou/03c04f10-4807-43f1-8e7e-1e43c048ef34";
+  "/orbitdb/zdpuAt4noCPbhfzRAZncCMndUPSCiYS5KnHDCPg17RFqV8qou";
 export const runukRujalojRubiTzib =
-  "/orbitdb/zdpuApC8sYxrezNAdgNFcLDcrEZhzMi7pv5A7VMZrCCiv2rb6/e45c68b2-2387-4092-b6be-3c50b3a5e884";
+  "/orbitdb/zdpuApC8sYxrezNAdgNFcLDcrEZhzMi7pv5A7VMZrCCiv2rb6";
 export const runukRujalojRucholanemTzib =
-  "/orbitdb/zdpuB14vFuP6aHNaXxoeWZvq1NRyx6HCUReoT4sxSk3kAFdZe/026952f2-38f5-4d18-95ca-d99114e2cc1e";
+  "/orbitdb/zdpuB14vFuP6aHNaXxoeWZvq1NRyx6HCUReoT4sxSk3kAFdZe";
 export const runukRujalojRutzibTzib =
-  "/orbitdb/zdpuAnXpasEMduNSXHxaeyGdZGmyU4NEwA3SEydj7jCDCY3Kz/3212b086-1480-4c9a-aa0e-0b5e98247ce4";
+  "/orbitdb/zdpuAnXpasEMduNSXHxaeyGdZGmyU4NEwA3SEydj7jCDCY3Kz";
